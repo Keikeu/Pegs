d5f60f9d53bcb2fc1c8040bd3392e2ba8eec499e
Keikeu <karolina.m.placek@gmail.com>
2019-04-01 22:19:35 +0200

    Updates

---
+++ precache-manifest.a77724efb8230fb88c7ac1fc42360a30.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2fa89b9237e56e8419686a0a88d1780",
+    "url": "/Pegs/static/media/vaporwave.f2fa89b9.mp3"
+  },
+  {
+    "revision": "d6bd60332652a74384626f3eb86ac089",
+    "url": "/Pegs/static/media/vaporwave.d6bd6033.jpg"
+  },
+  {
+    "revision": "ba917054e4524f344b3c5c7fda2dcdbc",
+    "url": "/Pegs/static/media/rules.ba917054.png"
+  },
+  {
+    "revision": "03f55f644f0c8e54f71449c4f79cbec5",
+    "url": "/Pegs/static/media/christmas.03f55f64.mp3"
+  },
+  {
+    "revision": "57aba3296a7997c657c8",
+    "url": "/Pegs/static/js/runtime~main.57aba329.js"
+  },
+  {
+    "revision": "f8a35b48f8d38d5fbca6",
+    "url": "/Pegs/static/js/main.f8a35b48.chunk.js"
+  },
+  {
+    "revision": "2df248d14e3b37a1817a",
+    "url": "/Pegs/static/js/1.2df248d1.chunk.js"
+  },
+  {
+    "revision": "f8a35b48f8d38d5fbca6",
+    "url": "/Pegs/static/css/main.eae5a7a8.chunk.css"
+  },
+  {
+    "revision": "ea9e0e583ac22004f09841869610aa29",
+    "url": "/Pegs/index.html"
+  }
+];
