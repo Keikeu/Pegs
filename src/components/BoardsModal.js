bd7b7b6e0d38d718fe687844d8d07ed8aabf8791
Keikeu <karolina.m.placek@gmail.com>
2023-10-31 21:04:58 +0100

    feat: display all board choices at once

--- src/components/BoardsModal.js
+++ src/components/BoardsModal.js
@@ -1,7 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 import T from "prop-types";
 import Button from "@material-ui/core/Button";
-import AliceCarousel from "react-alice-carousel";
 import tutorial from "../media/tutorial.png";
 import asymmetrical from "../media/asymmetrical.png";
 import diamond from "../media/diamond.png";
@@ -10,37 +9,10 @@ import english from "../media/english.png";
 import square from "../media/square.png";
 import Icon from "./Icon";
 
-const items = [tutorial, english, german, asymmetrical, square, diamond];
-const itemsNames = ["Tutorial", "English", "German", "Asymmetrical", "Square", "Diamond"];
+const boardImages = [tutorial, english, german, asymmetrical, square, diamond];
+const boardNames = ["Tutorial", "English", "German", "Asymmetrical", "Square", "Diamond"];
 
 const BoardsModal = ({ initialIndex, onClose, onClick }) => {
-  const [currentIndex, setCurrentIndex] = useState(initialIndex || 0);
-
-  const onSlideChanged = (e) => setCurrentIndex(e.item);
-
-  const slideNext = () => setCurrentIndex((index) => index + 1);
-  const slidePrev = () => setCurrentIndex((index) => index - 1);
-
-  function renderGallery() {
-    return (
-      <AliceCarousel
-        fadeOutAnimation={true}
-        dotsDisabled={true}
-        buttonsDisabled={true}
-        slideToIndex={currentIndex}
-        startIndex={currentIndex}
-        onSlideChanged={onSlideChanged}
-      >
-        {items.map((item, i) => (
-          <div key={itemsNames[i]}>
-            <h3>{itemsNames[i]}</h3>
-            <img className="board-img" src={item} alt={itemsNames[i]} />
-          </div>
-        ))}
-      </AliceCarousel>
-    );
-  }
-
   return (
     <div>
       <div className="dialog-container" onClick={onClose}></div>
@@ -49,16 +21,18 @@ const BoardsModal = ({ initialIndex, onClose, onClick }) => {
         <Button className="close-btn" onClick={onClose}>
           <Icon name="close" />
         </Button>
-        <Button className="carousel-btn" onClick={() => slidePrev()}>
-          <Icon name="chevron_left" />
-        </Button>
-        <Button className="carousel-btn" onClick={() => slideNext()}>
-          <Icon name="chevron_right" />
-        </Button>
-        {renderGallery()}
-        <Button className="play-btn" variant="contained" onClick={() => onClick(currentIndex)}>
-          Play
-        </Button>
+        <div className="board-gallery">
+          {boardImages.map((item, i) => (
+            <div
+              key={boardNames[i]}
+              className={`gallery-item${initialIndex === i ? " gallery-item--current" : ""}`}
+              onClick={() => onClick(i)}
+            >
+              <h3>{boardNames[i]}</h3>
+              <img className="board-img" src={item} alt={boardNames[i]} />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
